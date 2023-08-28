37041e5d2fc8c91d2bef4cbacce647dffc000a62
megz15 <meghraj.g16@gmail.com>
2023-08-28 14:42:39 +0530

    add: class constructor

--- class.dart
+++ class.dart
@@ -25,7 +25,7 @@ class SomeClassName {
 
 SomeClassName someObjectName = new SomeClassName();
 
-// [2] === Fields (Instance Members) () === [2] \\
+// [2] === Fields (Instance Members) === [2] \\
 
 // These are variables that can hold the state or data of an Object.
 // They represent the properties a class can have.
@@ -48,6 +48,41 @@ classFields() {
   """);
 }
 
+// [3] === Class Constructors === [3] \\
+
+// In many cases, you might need to pass your own data to Classes
+// like arguments to a function when initializing an Object
+
+// This is where a Constructor comes in. Constructors are
+// functions that runs when an Object is initialized
+
+// For passing data into a class, you'd define a function with the
+// same name as your class, then add `this.field` to reference a
+// positional argument passed to the constructor
+
+class MusicBand {
+  String name;
+  int formationYear;
+  String debutAlbum;
+  String genre;
+
+  MusicBand(this.name, this.formationYear, this.debutAlbum, this.genre);
+}
+
+// These variables can be accessed using the dot operator on Objects
+MusicBand anirudhVarmaCollective = MusicBand(
+  "Anirudh Varma Collective", 2016, "Perspectives", "Indian Classical Fusion / Indie"
+);
+
+classConstructor() {
+  print("""
+    The band ${anirudhVarmaCollective.name} was formed in ${anirudhVarmaCollective.formationYear}.
+    Their debut album was ${anirudhVarmaCollective.debutAlbum}.
+    They make songs of the genre ${anirudhVarmaCollective.genre}.
+  """);
+}
+
 main() {
   classFields(); //2
+  classConstructor(); //3
 }
