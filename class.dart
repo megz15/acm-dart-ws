1d6432b9c231647eca7ab605a54b82b694c02b00
megz15 <meghraj.g16@gmail.com>
2023-08-28 14:19:57 +0530

    add: class fields

--- class.dart
+++ class.dart
@@ -24,3 +24,30 @@ class SomeClassName {
 // with its value set as the class name followed by parentheses (like a function)
 
 SomeClassName someObjectName = new SomeClassName();
+
+// [2] === Fields (Instance Members) () === [2] \\
+
+// These are variables that can hold the state or data of an Object.
+// They represent the properties a class can have.
+
+class DaftPunk {
+  int formationYear = 1993;
+  String debutAlbum = "Homework";
+  bool wasBestBand = true;
+}
+
+// These variables can be accessed using the dot operator on Objects
+DaftPunk daftPunkObject = DaftPunk();
+
+classFields() {
+  print("""
+    Daft Punk was formed in ${daftPunkObject.formationYear}.
+    Their debut album was ${daftPunkObject.debutAlbum}.
+    It is ${daftPunkObject.wasBestBand} that they were one of the
+    best duos in the field of electronic music.
+  """);
+}
+
+main() {
+  classFields(); //2
+}
